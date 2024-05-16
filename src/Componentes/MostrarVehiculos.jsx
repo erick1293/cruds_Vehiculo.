71c168ec9e438359d32980ffae2cab8ffa2580d5
erick1293 <erick.meneses1293@gmail.com>
2024-05-16 15:45:07 -0400

    "Refactor MostrarVehiculos component to display vehicle list in a table format instead of a simple list"

--- src/Componentes/MostrarVehiculos.jsx
+++ src/Componentes/MostrarVehiculos.jsx
@@ -3,13 +3,34 @@ import React from 'react';
 const MostrarVehiculos = ({ vehiculos }) => {
     return (
         <div>
-            <h2>Lista de Vehículos</h2>
-            <ul>
-                {vehiculos.map(v => (
-                    <li key={v.id}>{v.id} {v.marca} {v.modelo} {v.anio}{v.patente}</li>
-                ))}
-            </ul>
-        </div>
+        <h2>Eliminar Vehículo</h2>
+        <ul>
+            {vehiculos.map(v => (
+                <li key={v.id}>
+
+                    <Table>
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>MARCA</th>
+                                <th>MODELO</th>
+                                <th>AÑO</th>
+                                <th>PATENTE</th>
+                            </tr>
+                        </thead>
+                        <thead>
+                            <tr>
+                                <th> {v.id} </th>
+                                <th>{v.marca}</th>
+                                <th>  {v.modelo}</th>
+                                <th>  {v.anio}</th>
+                                <th> {v.patente}</th>
+                            </tr>
+                        </thead></Table>  
+                </li>
+            ))}
+        </ul>
+    </div>
     );
 };
 
