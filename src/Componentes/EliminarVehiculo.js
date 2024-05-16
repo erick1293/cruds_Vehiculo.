5e074bea02c27a66bf3ee11e4eaa5d4233c91747
erick1293 <erick.meneses1293@gmail.com>
2024-05-16 14:40:43 -0400

    "Refactored EliminarVehiculo component to move Table component inside map function and added action column"

--- src/Componentes/EliminarVehiculo.js
+++ src/Componentes/EliminarVehiculo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Table from 'react-bootstrap/Table';{}
 
 const EliminarVehiculo = ({ vehiculos, setVehiculos }) => {
     const handleDelete = (id) => {
@@ -12,6 +13,32 @@ const EliminarVehiculo = ({ vehiculos, setVehiculos }) => {
             <ul>
                 {vehiculos.map(v => (
                     <li key={v.id}>
+
+<Table>
+      <thead>
+          <tr>
+              <th>ID</th>
+              <th>MARCA</th>
+              <th>MODELO</th>
+              <th>AÑO</th>
+              <th>PATENTE</th>
+          </tr>
+      </thead>
+      <tbody>
+          {vehiculos.map(({id,marca,modelo,anio,patente})=>
+              (
+                <tr>
+                  <td>{id}</td>
+                  <td>{marca}</td>
+                  <td>{modelo}</td>
+                  <td>{anio}</td>
+                  <td>{patente}</td>  
+              </tr>
+              )
+          )}
+      </tbody>
+  </Table>
+
                         {v.marca} {v.modelo} <button onClick={() => handleDelete(v.id)}>Eliminar</button>
                     </li>
                 ))}
