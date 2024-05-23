e02bf8599694b299be9523b3157d1ddf14140bb8
erick1293 <erick.meneses1293@gmail.com>
2024-05-23 13:59:24 -0400

    .

--- src/Componentes/Conexion/database.php
+++ src/Componentes/Conexion/database.php
@@ -1,9 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: *");
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
 $servername = "localhost";
 $username = "root";
